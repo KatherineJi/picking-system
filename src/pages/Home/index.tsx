4ed74eaa5fc9246a3ed41c6689dbf6423e2492a4
墨墨 <momo@s-MacBook-Pro-3.local>
2025-04-10 17:19:34 +0800

    feat: update homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,12 @@
+import SearchPanel from '@/components/SearchPanel';
+import MapPanel from '@/components/MapPanel';
+
 const Home = () => {
   return (
-    <>Home</>
+    <div className='home-page flex bg-stone-200 h-screen'>
+      <SearchPanel />
+      <MapPanel />
+    </div>
   );
 };
 
